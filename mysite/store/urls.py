d7721000bccf407c5c9877eecf47d67118c8a448
kiana <kianasasannia@gmail.com>
2026-06-30 20:17:31 +0330

    version2

--- mysite/store/urls.py
+++ mysite/store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns =[
-    path('',views.product_list,name='product_list')
+    path('',views.product_list,name='product_list'),
+    path('product/<int:id>/',views.product_detail,name="product_detail"),
+    path('category/<int:category_id>/',views.products_by_category,name="products_by_category")
 ]
--- mysite/store/views.py
+++ mysite/store/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Category
 
 def product_list (request):
     products = Product.objects.all()
-    return render(request,'store/product_list.html',{'products':products})
+    categories=Category.objects.all()
+    return render(request,'store/product_list.html',{'products':products,"categories":categories})
 
+def product_detail(request,id):
+    product=get_object_or_404(Product,id=id)
+    return render(request,'product_detail.html',{'product':product})
+
+
+def products_by_category (request ,category_id):
+    categories=Category.objects.all()
+    selected_category=get_object_or_404(Category,id=category_id)
+    products=Product.objects.filter(category=selected_category)
+    return render (request,"store/product_list.html",{"products":products,"categories":categories,"selected_category":selected_category})
